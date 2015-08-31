36764d968367359d01b85b5b4fdb4d8e4412dd7d
Seth Miller <seth@sethmiller.me>
2015-08-31 13:16:55 -0500

    initial commit

---
+++ akcli/cli.py
@@ -0,0 +1,97 @@
+import ConfigParser
+import json
+from os.path import expanduser
+import pprint
+
+import click
+
+from dns import AkamaiDNS
+
+SUPPORTED_RECORD_TYPES = ['A', 'CNAME', 'NS']
+
+
+class Context(object):
+    pass
+
+
+@click.group()
+@click.option('--verbose', '-v', is_flag=True, help='Enables verbose mode.')
+@click.option('--debug', is_flag=True, help='Enables debug mode.')
+@click.option('--json', is_flag=True, help='Output as JSON.')
+@click.option('--config', type=click.File(), default='{}/.akamai.cfg'.format(expanduser("~")))
+@click.version_option('1.0')
+@click.pass_context
+def cli(ctx, verbose, debug, json, config):
+    ctx.obj = Context()
+    ctx.obj.verbose = verbose
+    ctx.obj.debug = debug
+    ctx.obj.json = json
+    ctx.obj.config = ConfigParser.ConfigParser()
+    ctx.obj.config.readfp(config)
+
+
+@cli.group()
+@click.pass_context
+def dns(ctx):
+    baseurl = ctx.obj.config.get('auth', 'baseurl')
+    client_token = ctx.obj.config.get('auth', 'client_token')
+    client_secret = ctx.obj.config.get('auth', 'client_secret')
+    access_token = ctx.obj.config.get('auth', 'access_token')
+    ctx.obj.akamai_dns = AkamaiDNS(baseurl, client_token, client_secret, access_token)
+
+
+@dns.command()
+@click.argument('zone')
+@click.argument('name')
+@click.argument('type', type=click.Choice(SUPPORTED_RECORD_TYPES))
+@click.argument('target')
+@click.option('--ttl', type=click.INT, default=600, help='TTL value in seconds, such as 86400')
+@click.pass_context
+def add_record(ctx, zone, name, type, target, ttl):
+    successful = ctx.obj.akamai_dns.add_record(zone_name=zone, record_type=type, name=name, target=target, ttl=ttl)
+    if successful:
+        click.echo(json.dumps("Record added successfully."))
+    else:
+        click.echo(json.dumps("Failed to add record."))
+
+
+@dns.command()
+@click.argument('zone')
+@click.argument('name')
+@click.argument('type', type=click.Choice(SUPPORTED_RECORD_TYPES))
+@click.pass_context
+def fetch_record(ctx, zone, name, type):
+    record = ctx.obj.akamai_dns.fetch_record(zone_name=zone, record_type=type, name=name)
+    if ctx.obj.json:
+        click.echo(json.dumps(record))
+    else:
+        click.echo(record)
+
+
+@dns.command()
+@click.argument('zone')
+@click.pass_context
+def fetch_zone(ctx, zone):
+    zone = ctx.obj.akamai_dns.fetch_zone(zone_name=zone)
+    if ctx.obj.json:
+        click.echo(json.dumps(zone))
+    else:
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(zone)
+
+
+@dns.command()
+@click.argument('zone')
+@click.argument('type', required=False, type=click.Choice(SUPPORTED_RECORD_TYPES))
+@click.pass_context
+def list_records(ctx, zone, type):
+    records = ctx.obj.akamai_dns.list_records(zone_name=zone, record_type=type)
+    if ctx.obj.json:
+        click.echo(json.dumps(records))
+    else:
+        for record in records:
+            click.echo('{name:30}{type:20}{target:20}{ttl:10}'.format(**record))
+
+
+if __name__ == '__main__':
+    cli()
---
+++ akcli/dns.py
@@ -0,0 +1,120 @@
+import json
+from urlparse import urljoin
+import logging
+
+import requests
+from akamai.edgegrid import EdgeGridAuth
+
+log = logging.getLogger(__name__)
+
+
+class AkamaiDNSError(Exception):
+    pass
+
+
+class AkamaiDNS(object):
+    def __init__(self, baseurl, client_token, client_secret, access_token):
+        self.baseurl = baseurl
+        self.session = requests.Session()
+        self.session.auth = EdgeGridAuth(client_token=client_token,
+                                         client_secret=client_secret,
+                                         access_token=access_token,
+                                         max_body=128 * 1024)
+
+    def _update_zone(self, zone_name):
+        '''Update a given Zone. This method will handle incrementing the SOA serial for you.
+
+        :param zone_name: This is the name of the zone e.g. example.com.
+        :return:
+        '''
+        # Increment the serial.
+        name = zone_name['zone']['name']
+        zone_name['zone']['soa']['serial'] += 1
+
+        # POST the updated zone file.
+        url = urljoin(self.baseurl, '/config-dns/v1/zones/{0}'.format(name))
+        headers = {'content-type': 'application/json'}
+        result = self.session.post(url, data=json.dumps(zone_name), headers=headers)
+
+        if result.status_code != 204:
+            log.error('Something went very wrong with updating the zone "%s"', zone_name)
+        return True
+
+    def fetch_zone(self, zone_name):
+        '''Fetch a dictionary representation of a zone file.
+
+        :param zone_name: This is the name of the zone e.g. example.com.
+        :return: A dict containing a zone.
+        '''
+        url = urljoin(self.baseurl, '/config-dns/v1/zones/{0}'.format(zone_name))
+        result = self.session.get(url)
+        if result.status_code == 404:
+            log.error('Zone "%s" not found.', zone_name)
+            return None
+        return result.json()
+
+    def list_records(self, zone_name, record_type=None):
+        records = []
+        zone = self.fetch_zone(zone_name)['zone']
+        if record_type:
+            record_type = record_type.lower()
+            for record in zone.get(record_type):
+                record['type'] = record_type.upper()
+                records.append(record)
+        else:
+
+            for key, val in zone.items():
+                if type(val) is list:
+                    for record in val:
+                        record['type'] = key.upper()
+                        records.append(record)
+        return records
+
+    def fetch_record(self, zone_name, record_type, name):
+        '''Fetch a dictionary representation of a particular DNS record.
+
+        :param zone_name: This is the name of the zone e.g. example.com.
+        :param name: This is the "from" section. That is for web01.example.com, this would be web01.
+        :param record_type:
+        :return:
+        '''
+
+        for record in self.list_records(zone_name, record_type):
+            if record['name'] == name:
+                return record
+        return None
+
+    def record_exists(self, zone_name, record_type, name):
+        '''Determine if a particular DNS record already exists.
+
+        :param zone_name: Name of the zone to add a record to.
+        :param record_type: Type of record (a, cname, ptr, etc).
+        :param name: This is the "from" section. That is for web01.example.com, this would be web01.
+        :return: True if record found, otherwise False.
+        '''
+        record_type = record_type.lower()
+        if self.fetch_record(zone_name=zone_name, record_type=record_type, name=name):
+            return True
+        return False
+
+    def add_record(self, zone_name, record_type, name, target, ttl=600):
+        '''Add a new DNS record to a zone.
+
+        :param zone_name: Name of the zone to add a record to.
+        :param record_type: Type of record (a, cname, ptr, etc).
+        :param name: This is the "from" section. That is for web01.example.com, this would be web01.
+        :param target: This is the "to" section e.g. "10.0.0.1".
+        :param ttl: The DNS record time to live value. Defaults to 600.
+        :return: Returns True if successful, otherwise False.
+        '''
+        record_type = record_type.lower()
+        zone = self.fetch_zone(zone_name)
+        if not zone:
+            raise AkamaiDNSError('Zone {0} not found.'.format(zone_name))
+
+        if self.record_exists(zone_name=zone_name, record_type=record_type, name=name):
+            return True
+
+        new_record = {'name': name, 'target': target, 'ttl': ttl, 'active': True}
+        zone['zone'].get(record_type).append(new_record)
+        return self._update_zone(zone)
